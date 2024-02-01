1d74db9a99ee7643d9ee5ef0c1d53ef399e9ac28
Lukasz <lukasz.maniak22@gmail.com>
2024-02-01 21:29:05 +0100

    main

--- src/components/AttributionP.js
+++ src/components/AttributionP.js
@@ -1,16 +1,18 @@
+import styles from "./AttributionP.module.css";
+
 export default function AttributionP() {
   return (
-    <p className="attribution-p">
+    <p className={styles["attribution-p"]}>
       Challenge by{" "}
       <a
-        className="attribution-link"
+        className={styles["attribution-link"]}
         href="https://www.frontendmentor.io/challenges/intro-section-with-dropdown-navigation-ryaPetHE5"
       >
         Frontend Mentor
       </a>
       . Coded by{" "}
       <a
-        className="attribution-link"
+        className={styles["attribution-link"]}
         href="https://github.com/LukaszManiak"
         role="button"
       >
--- src/components/Country.js
+++ src/components/Country.js
@@ -1,14 +1,18 @@
+import styles from "./Country.module.css";
+
 export default function Country({ country, onCountrySelect, isDarkMode }) {
   return (
     <div
       onClick={() => onCountrySelect(country.name.common)}
       className={
-        !isDarkMode ? "country-box bright-mode" : "country-box dark-mode"
+        !isDarkMode
+          ? `${styles["country-box"]} ${styles["bright-mode"]}`
+          : `${styles["country-box"]} ${styles["dark-mode"]}`
       }
     >
       <img src={country.flags.svg} alt="country flag" />
-      <div className="country-box-detail">
-        <p className="country-name">{country.name.common}</p>
+      <div className={styles["country-box-detail"]}>
+        <p className={styles["country-name"]}>{country.name.common}</p>
         <p>
           <b>Population</b>: {country.population}
         </p>
